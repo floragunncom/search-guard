f35556f5d5eeef7732600d2fc9cafce9f9e1c00c
floragunn <dev@search-guard.com>
2020-01-11 19:10:23 +0100

    add "-s settings.xml" to mvn (because we use currently a private pwd protected repo) also in smoketest.sh

--- smoketest.sh
+++ smoketest.sh
@@ -17,7 +17,7 @@ fi
 tar -xzf elasticsearch-$ES_VERSION-darwin-x86_64.tar.gz
 rm -rf elasticsearch-$ES_VERSION-darwin-x86_64.tar.gz
 #wget -O netty-tcnative-$NETTY_NATIVE_VERSION-$NETTY_NATIVE_CLASSIFIER.jar https://search.maven.org/remotecontent?filepath=io/netty/netty-tcnative/$NETTY_NATIVE_VERSION/netty-tcnative-$NETTY_NATIVE_VERSION-$NETTY_NATIVE_CLASSIFIER.jar
-mvn clean package -Penterprise -DskipTests
+mvn clean package -Penterprise -DskipTests -s settings.xml
 PLUGIN_FILE=($DIR/target/releases/search-guard!(*sgadmin*).zip)
 URL=file://$PLUGIN_FILE
 echo $URL
