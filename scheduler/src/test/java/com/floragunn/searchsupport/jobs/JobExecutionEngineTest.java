07e6a5699266d8adcf2be80a35b72b59ccf7f77d
Nils Bandener <git@bndnr.de>
2020-01-11 22:08:41 +0100

    Retired most test for the JobExecutionEngine.
    These tests should be really integration tests.

--- scheduler/src/test/java/com/floragunn/searchsupport/jobs/JobExecutionEngineTest.java
+++ scheduler/src/test/java/com/floragunn/searchsupport/jobs/JobExecutionEngineTest.java
@@ -3,12 +3,7 @@ package com.floragunn.searchsupport.jobs;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -21,6 +16,7 @@ import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.node.PluginAwareNode;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
@@ -36,144 +32,6 @@ import com.floragunn.searchsupport.jobs.config.DefaultJobConfig;
 
 public class JobExecutionEngineTest extends SingleClusterTest {
     private static final Logger log = LogManager.getLogger(JobExecutionEngineTest.class);
-
-    @Test
-    public void test() throws Exception {
-        final Settings settings = Settings.builder().build();
-
-        setup(settings);
-
-        try (Client tc = getInternalTransportClient()) {
-
-            tc.index(new IndexRequest("testjobconfig").setRefreshPolicy(RefreshPolicy.IMMEDIATE)
-                    .source("{\"hash\": 1, \"trigger\": {\"schedule\": {\"cron\": \"*/2 * * * * ?\"}}}", XContentType.JSON)).actionGet();
-
-            for (PluginAwareNode node : this.clusterHelper.allNodes()) {
-                ClusterService clusterService = node.injector().getInstance(ClusterService.class);
-
-                Scheduler scheduler = new SchedulerBuilder<DefaultJobConfig>().client(tc).name("test_" + clusterService.getNodeName())
-                        .configIndex("testjobconfig").jobConfigFactory(new ConstantHashJobConfig.Factory(LoggingTestJob.class))
-                        .distributed(clusterService).nodeComparator(new NodeNameComparator(clusterService)).build();
-
-                scheduler.start();
-            }
-
-            Thread.sleep(15 * 1000);
-
-            this.clusterHelper.allNodes().get(1).close();
-
-            Thread.sleep(15 * 1000);
-
-            clusterHelper.stopCluster();
-
-        }
-
-    }
-
-    @Test
-    public void basicTest() throws Exception {
-        final Settings settings = Settings.builder().build();
-
-        setup(settings);
-
-        try (Client tc = getInternalTransportClient()) {
-
-            String jobConfig = createCronJobConfig(1, "basic", null, "*/1 * * * * ?");
-
-            tc.index(new IndexRequest("testjobconfig").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source(jobConfig, XContentType.JSON)).actionGet();
-
-            for (PluginAwareNode node : this.clusterHelper.allNodes()) {
-                ClusterService clusterService = node.injector().getInstance(ClusterService.class);
-
-                Scheduler scheduler = new SchedulerBuilder<DefaultJobConfig>().client(tc).name("test_" + clusterService.getNodeName())
-                        .configIndex("testjobconfig").jobConfigFactory(new ConstantHashJobConfig.Factory(TestJob.class)).distributed(clusterService)
-                        .nodeComparator(new NodeNameComparator(clusterService)).build();
-
-                scheduler.start();
-            }
-
-            Thread.sleep(5 * 1000);
-
-            int count = TestJob.getCounter("basic");
-
-            assertTrue("count is " + count, count >= 4 && count <= 8);
-
-            clusterHelper.stopCluster();
-
-        }
-
-    }
-
-    @Test
-    public void nonConcurrentTest() throws Exception {
-        final Settings settings = Settings.builder().build();
-
-        setup(settings);
-
-        try (Client tc = getInternalTransportClient()) {
-
-            String jobConfig = createCronJobConfig(1, "nonconcurrent", 1500, "*/1 * * * * ?");
-
-            tc.index(new IndexRequest("testjobconfig").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source(jobConfig, XContentType.JSON)).actionGet();
-
-            for (PluginAwareNode node : this.clusterHelper.allNodes()) {
-                ClusterService clusterService = node.injector().getInstance(ClusterService.class);
-
-                Scheduler scheduler = new SchedulerBuilder<DefaultJobConfig>().client(tc).name("test_" + clusterService.getNodeName())
-                        .configIndex("testjobconfig").jobConfigFactory(new ConstantHashJobConfig.Factory(NonConcurrentTestJob.class))
-                        .distributed(clusterService).nodeComparator(new NodeNameComparator(clusterService)).build();
-
-                scheduler.start();
-            }
-
-            Thread.sleep(5 * 1000);
-
-            assertEquals(1, TestJob.getCounter("nonconcurrent_max_concurrency"));
-
-            int count = TestJob.getCounter("nonconcurrent");
-
-            assertTrue("count is " + count, count >= 4 && count <= 8);
-
-            clusterHelper.stopCluster();
-
-        }
-
-    }
-
-    @Test
-    public void nodeFilterTest() throws Exception {
-        final Settings settings = Settings.builder().build();
-
-        setup(settings);
-
-        try (Client tc = getInternalTransportClient()) {
-
-            String jobConfig = createCronJobConfig(1, "nodeFilterTest", null, "*/1 * * * * ?");
-
-            tc.index(new IndexRequest("testjobconfig").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source(jobConfig, XContentType.JSON)).actionGet();
-
-            for (PluginAwareNode node : this.clusterHelper.allNodes()) {
-                ClusterService clusterService = node.injector().getInstance(ClusterService.class);
-
-                Scheduler scheduler = new SchedulerBuilder<DefaultJobConfig>().client(tc).name("test_" + clusterService.getNodeName())
-                        .nodeFilter("node_group_1:a").configIndex("testjobconfig").jobConfigFactory(new ConstantHashJobConfig.Factory(TestJob.class))
-                        .distributed(clusterService).nodeComparator(new NodeNameComparator(clusterService)).build();
-
-                scheduler.start();
-
-            }
-
-            Thread.sleep(5 * 1000);
-
-            int count = TestJob.getCounter("nodeFilterTest");
-
-            assertTrue("count is " + count, count >= 4 && count <= 8);
-
-            clusterHelper.stopCluster();
-
-        }
-
-    }
 
     @Test
     public void emptyNodeFilterTest() throws Exception {
@@ -199,7 +57,7 @@ public class JobExecutionEngineTest extends SingleClusterTest {
 
             }
 
-            Thread.sleep(5 * 1000);
+            Thread.sleep(3 * 1000);
 
             int count = TestJob.getCounter("emptyNodeFilterTest");
 
@@ -211,6 +69,7 @@ public class JobExecutionEngineTest extends SingleClusterTest {
 
     }
 
+    @Ignore
     @Test
     public void configUpdateTest() throws Exception {
         final Settings settings = Settings.builder().build();
@@ -251,6 +110,7 @@ public class JobExecutionEngineTest extends SingleClusterTest {
         }
     }
 
+    @Ignore
     @Test
     public void triggerUpdateTest() throws Exception {
         final Settings settings = Settings.builder().build();
@@ -261,7 +121,8 @@ public class JobExecutionEngineTest extends SingleClusterTest {
 
             String jobConfig = createCronJobConfig(1, "basic", null, "*/1 * * * * ?");
 
-            tc.index(new IndexRequest("testjobconfig").id("trigger_update_test_job").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source(jobConfig, XContentType.JSON)).actionGet();
+            tc.index(new IndexRequest("testjobconfig").id("trigger_update_test_job").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source(jobConfig,
+                    XContentType.JSON)).actionGet();
 
             for (PluginAwareNode node : this.clusterHelper.allNodes()) {
                 if ("1".equals(node.settings().get("node.attr.node_index"))) {
@@ -281,7 +142,8 @@ public class JobExecutionEngineTest extends SingleClusterTest {
             int count1 = TestJob.getCounter("basic");
 
             jobConfig = createCronJobConfig(1, "basic", null, "* * * * * ?");
-            tc.index(new IndexRequest("testjobconfig").id("trigger_update_test_job").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source(jobConfig, XContentType.JSON)).actionGet();
+            tc.index(new IndexRequest("testjobconfig").id("trigger_update_test_job").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source(jobConfig,
+                    XContentType.JSON)).actionGet();
             SchedulerConfigUpdateAction.send(tc, "test");
 
             Thread.sleep(3 * 1000);
@@ -293,119 +155,6 @@ public class JobExecutionEngineTest extends SingleClusterTest {
             assertTrue("count is " + count2, count2 > count1);
 
         }
-    }
-
-    @Test
-    public void weeklyScheduleTest() throws Exception {
-        final Settings settings = Settings.builder().build();
-
-        setup(settings);
-
-        try (Client tc = getInternalTransportClient()) {
-
-            LocalDate localDate = LocalDate.now();
-            LocalTime localTime = LocalTime.now();
-
-            String jobConfig = createJobConfig(1, "weekly_schedule", null, "weekly", "on", localDate.getDayOfWeek().toString().toLowerCase(), "at",
-                    localTime.plus(Duration.ofSeconds(5)).truncatedTo(ChronoUnit.SECONDS));
-
-            tc.index(new IndexRequest("testjobconfig").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source(jobConfig, XContentType.JSON)).actionGet();
-
-            for (PluginAwareNode node : this.clusterHelper.allNodes()) {
-                ClusterService clusterService = node.injector().getInstance(ClusterService.class);
-
-                Scheduler scheduler = new SchedulerBuilder<DefaultJobConfig>().client(tc).name("test_" + clusterService.getNodeName())
-                        .configIndex("testjobconfig").jobConfigFactory(new ConstantHashJobConfig.Factory(TestJob.class)).distributed(clusterService)
-                        .nodeComparator(new NodeNameComparator(clusterService)).build();
-
-                scheduler.start();
-            }
-
-            Thread.sleep(10 * 1000);
-
-            int count = TestJob.getCounter("weekly_schedule");
-
-            assertEquals(1, count);
-
-            clusterHelper.stopCluster();
-
-        }
-
-    }
-
-    @Test
-    public void monthlyScheduleTest() throws Exception {
-        final Settings settings = Settings.builder().build();
-
-        setup(settings);
-
-        try (Client tc = getInternalTransportClient()) {
-
-            LocalDate localDate = LocalDate.now();
-            LocalTime localTime = LocalTime.now();
-
-            String jobConfig = createJobConfig(1, "monthly_schedule", null, "monthly", "on", localDate.getDayOfMonth(), "at",
-                    localTime.plus(Duration.ofSeconds(5)).truncatedTo(ChronoUnit.SECONDS));
-
-            tc.index(new IndexRequest("testjobconfig").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source(jobConfig, XContentType.JSON)).actionGet();
-
-            for (PluginAwareNode node : this.clusterHelper.allNodes()) {
-                ClusterService clusterService = node.injector().getInstance(ClusterService.class);
-
-                Scheduler scheduler = new SchedulerBuilder<DefaultJobConfig>().client(tc).name("test_" + clusterService.getNodeName())
-                        .configIndex("testjobconfig").jobConfigFactory(new ConstantHashJobConfig.Factory(TestJob.class)).distributed(clusterService)
-                        .nodeComparator(new NodeNameComparator(clusterService)).build();
-
-                scheduler.start();
-            }
-
-            Thread.sleep(10 * 1000);
-
-            int count = TestJob.getCounter("monthly_schedule");
-
-            assertEquals(1, count);
-
-            clusterHelper.stopCluster();
-
-        }
-
-    }
-
-    @Test
-    public void dailyScheduleTest() throws Exception {
-        final Settings settings = Settings.builder().build();
-
-        setup(settings);
-
-        try (Client tc = getInternalTransportClient()) {
-
-            LocalTime localTime = LocalTime.now();
-
-            String jobConfig = createJobConfig(1, "daily_schedule", null, "daily", "at",
-                    localTime.plus(Duration.ofSeconds(5)).truncatedTo(ChronoUnit.SECONDS));
-
-            tc.index(new IndexRequest("testjobconfig").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source(jobConfig, XContentType.JSON)).actionGet();
-
-            for (PluginAwareNode node : this.clusterHelper.allNodes()) {
-                ClusterService clusterService = node.injector().getInstance(ClusterService.class);
-
-                Scheduler scheduler = new SchedulerBuilder<DefaultJobConfig>().client(tc).name("test_" + clusterService.getNodeName())
-                        .configIndex("testjobconfig").jobConfigFactory(new ConstantHashJobConfig.Factory(TestJob.class)).distributed(clusterService)
-                        .nodeComparator(new NodeNameComparator(clusterService)).build();
-
-                scheduler.start();
-            }
-
-            Thread.sleep(10 * 1000);
-
-            int count = TestJob.getCounter("daily_schedule");
-
-            assertEquals(1, count);
-
-            clusterHelper.stopCluster();
-
-        }
-
     }
 
     private String createCronJobConfig(int hash, String name, Integer delay, String... cronSchedule) {
@@ -441,56 +190,6 @@ public class JobExecutionEngineTest extends SingleClusterTest {
         }
 
         result.append("}}}");
-
-        return result.toString();
-    }
-
-    private String createJobConfig(int hash, String name, Integer delay, String scheduleType, Object... schedule) {
-        StringBuilder result = new StringBuilder("{");
-
-        result.append("\"hash\": ").append(hash).append(",");
-
-        if (name != null) {
-            result.append("\"name\": \"").append(name).append("\",");
-        }
-
-        if (delay != null) {
-            result.append("\"delay\": ").append(delay).append(",");
-        }
-
-        result.append("\"trigger\": {\"schedule\": {\"").append(scheduleType).append("\": {");
-
-        for (int i = 0; i < schedule.length; i += 2) {
-            if (i > 0) {
-                result.append(",");
-            }
-
-            result.append("\"").append(schedule[i]).append("\":");
-
-            if (schedule[i + 1] instanceof List) {
-                result.append("[");
-
-                boolean first = true;
-
-                for (Object element : (List<?>) schedule[i + 1]) {
-                    if (first) {
-                        first = false;
-                    } else {
-                        result.append(",");
-                    }
-
-                    result.append("\"").append(element).append("\"");
-                }
-
-                result.append("]");
-            } else if (schedule[i + 1] instanceof Number) {
-                result.append(schedule[i + 1]);
-            } else {
-                result.append("\"").append(schedule[i + 1]).append("\"");
-            }
-        }
-
-        result.append("}}}}");
 
         return result.toString();
     }
