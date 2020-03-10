fd3aef2f781d11093aba3b7aa63318538d7eb070
Nils Bandener <git@bndnr.de>
2020-03-10 15:34:21 +0100

    Removed bogus logging

--- signals/src/main/java/com/floragunn/signals/SignalsTenant.java
+++ signals/src/main/java/com/floragunn/signals/SignalsTenant.java
@@ -131,10 +131,6 @@ public class SignalsTenant implements Closeable {
     public void init() throws SchedulerException {
         log.info("Initializing alerting tenant " + name + "\nnodeFilter: " + nodeFilter);
 
-        SecurityManager s = System.getSecurityManager();
-
-        log.info("************ " + s.getThreadGroup() + " " + Thread.currentThread().getThreadGroup() + "  " + s.getSecurityContext());
-        
         this.scheduler = new SchedulerBuilder<Watch>().client(privilegedConfigClient).name(scopedName)
                 .configIndex(configIndexName, getActiveConfigQuery(name))
                 .stateIndex(settings.getStaticSettings().getIndexNames().getWatchesTriggerState()).stateIndexIdPrefix(watchIdPrefix)
