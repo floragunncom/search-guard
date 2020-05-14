0180437894883de3d26adbe87d99b0c8dd79075e
Qaiser Abbasi <qabbasi@search-guard.com>
2020-05-14 13:43:14 +0200

    debug failing test

--- dlic-security/src/test/java/com/floragunn/dlic/auth/ldap2/LdapBackendTestOldStyleConfig2.java
+++ dlic-security/src/test/java/com/floragunn/dlic/auth/ldap2/LdapBackendTestOldStyleConfig2.java
@@ -312,6 +312,8 @@ public class LdapBackendTestOldStyleConfig2 {
                     .authenticate(new AuthCredentials("jacksonm", "secret".getBytes(StandardCharsets.UTF_8)));
             Assert.fail("Expected Exception");
         } catch (Exception e) {
+            System.out.println("wh am i failing? " + e.toString());
+
             Assert.assertEquals(LDAPBindException.class, e.getClass());
         }
 
