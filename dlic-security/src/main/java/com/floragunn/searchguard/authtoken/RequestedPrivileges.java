d03b356dcd9bff8314304e0cd3611e00e910e016
Nils Bandener <git@bndnr.de>
2020-11-07 09:59:43 +0100

    Added copyright headers

--- dlic-security/src/main/java/com/floragunn/searchguard/authtoken/RequestedPrivileges.java
+++ dlic-security/src/main/java/com/floragunn/searchguard/authtoken/RequestedPrivileges.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2020 by floragunn GmbH - All rights reserved
+ * 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed here is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 
+ * This software is free of charge for non-commercial and academic use. 
+ * For commercial use in a production environment you have to obtain a license 
+ * from https://floragunn.com
+ * 
+ */
+
 package com.floragunn.searchguard.authtoken;
 
 import java.io.IOException;
