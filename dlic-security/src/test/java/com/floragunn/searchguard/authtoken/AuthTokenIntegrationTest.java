31e0596d3a5a7bab97b8c2c5f151bb01c3f351cc
Nils Bandener <9-nibix@users.noreply.git.floragunn.com>
2021-02-15 12:22:11 +0100

    Fixed revocation for non-special-privs case

--- dlic-security/src/test/java/com/floragunn/searchguard/authtoken/AuthTokenIntegrationTest.java
+++ dlic-security/src/test/java/com/floragunn/searchguard/authtoken/AuthTokenIntegrationTest.java
@@ -17,12 +17,14 @@ package com.floragunn.searchguard.authtoken;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
 
 import org.apache.http.Header;
 import org.apache.http.HttpStatus;
 import org.apache.http.message.BasicHeader;
+import org.elasticsearch.ElasticsearchStatusException;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
@@ -32,6 +34,7 @@ import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -462,13 +465,78 @@ public class AuthTokenIntegrationTest {
                             .source(SearchSourceBuilder.searchSource().query(QueryBuilders.matchAllQuery())), RequestOptions.DEFAULT);
                     Assert.fail(searchResponse.toString());
 
-                } catch (Exception e) {
-                    Assert.assertTrue(e.getMessage(), e.getMessage().contains("no permissions for [indices:data/read/search]"));
+                } catch (ElasticsearchStatusException e) {
+                    Assert.assertEquals(e.getMessage(), RestStatus.UNAUTHORIZED, e.status());
                 }
             }
         }
     }
 
+    @Test
+    public void revocationWithoutSpecialPrivsTest() throws Exception {
+        TestSgConfig sgConfig = AuthTokenIntegrationTest.sgConfig.clone().sgConfigSettings("sg_config.dynamic.auth_token_provider.exclude_cluster_permissions", Collections.emptyList());
+
+        try (LocalCluster cluster = new LocalCluster.Builder().nodeSettings("searchguard.restapi.roles_enabled.0", "sg_admin")
+                .resources("authtoken").sslEnabled().sgConfig(sgConfig).build()) {
+            
+            try (Client client = cluster.getInternalClient()) {
+                client.index(new IndexRequest("pub_test_allow_because_from_token").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source(XContentType.JSON,
+                        "this_is", "allowed")).actionGet();
+            }
+            
+            CreateAuthTokenRequest request = new CreateAuthTokenRequest(RequestedPrivileges.totalWildcard());
+            request.setTokenName("my_new_token_without_special_privs");
+            request.setFreezePrivileges(false);
+
+            Header auth = basicAuth("spock", "spock");
+            
+            RestHelper rh = cluster.restHelper();
+
+            HttpResponse response = rh.executePostRequest("/_searchguard/authtoken", request.toJson(), auth);
+
+            Assert.assertEquals(200, response.getStatusCode());
+
+            String token = response.toJsonNode().get("token").asText();
+            String id = response.toJsonNode().get("id").asText();
+
+            Assert.assertNotNull(token);
+            Assert.assertNotNull(id);
+
+            for (int i = 0; i < 3; i++) {
+                try (RestHighLevelClient client = cluster.getRestHighLevelClientForNode(i, new BasicHeader("Authorization", "Bearer " + token))) {
+                    SearchResponse searchResponse = client.search(new SearchRequest("pub_test_allow_because_from_token")
+                            .source(SearchSourceBuilder.searchSource().query(QueryBuilders.matchAllQuery())), RequestOptions.DEFAULT);
+
+                    Assert.assertEquals(1, searchResponse.getHits().getTotalHits().value);
+                    Assert.assertEquals("allowed", searchResponse.getHits().getAt(0).getSourceAsMap().get("this_is"));
+                }
+            }
+
+            response = rh.executeDeleteRequest("/_searchguard/authtoken/" + id, auth);
+            Assert.assertEquals(response.getBody(), 200, response.getStatusCode());
+            Thread.sleep(100);
+
+            for (int i = 0; i < 3; i++) {
+
+                try (RestHighLevelClient client = cluster.getRestHighLevelClientForNode(i, new BasicHeader("Authorization", "Bearer " + token))) {
+                    try {
+
+                        SearchResponse searchResponse = client.search(new SearchRequest("pub_test_allow_because_from_token")
+                                .source(SearchSourceBuilder.searchSource().query(QueryBuilders.matchAllQuery())), RequestOptions.DEFAULT);
+                        Assert.fail(searchResponse.toString());
+
+                    } catch (ElasticsearchStatusException e) {
+                        Assert.assertEquals(e.getMessage(), RestStatus.UNAUTHORIZED, e.status());
+                    }
+                }
+            }
+            
+        }   
+    }
+
+    
+
+    
     @Test
     public void getAndSearchTest() throws Exception {
         CreateAuthTokenRequest request = new CreateAuthTokenRequest(
