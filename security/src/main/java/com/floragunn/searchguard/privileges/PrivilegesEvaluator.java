9f02886538afd37479b087df796f81152bb46797
Nils Bandener <git@bndnr.de>
2021-09-23 14:13:22 +0200

    Throw meaningful exception when SG is not yet initialized
    Before we just ran into an NPE

--- security/src/main/java/com/floragunn/searchguard/privileges/PrivilegesEvaluator.java
+++ security/src/main/java/com/floragunn/searchguard/privileges/PrivilegesEvaluator.java
@@ -523,6 +523,10 @@ public class PrivilegesEvaluator implements DCFListener {
     }
 
     public Set<String> mapSgRoles(final User user, final TransportAddress caller) {
+        if (this.configModel == null) {
+            throw new ElasticsearchSecurityException("SearchGuard is not yet initialized");
+        }
+        
         return this.configModel.mapSgRoles(user, caller);
     }
 
