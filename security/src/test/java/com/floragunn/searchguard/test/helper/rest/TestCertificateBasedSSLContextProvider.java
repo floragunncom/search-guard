9bd186019cde173561e00fd6cb05cb1f85f5f830
Nils Bandener <git@bndnr.de>
2021-12-15 14:01:35 +0100

    Bugfix for test framework TLS handling

--- security/src/test/java/com/floragunn/searchguard/test/helper/rest/TestCertificateBasedSSLContextProvider.java
+++ security/src/test/java/com/floragunn/searchguard/test/helper/rest/TestCertificateBasedSSLContextProvider.java
@@ -17,22 +17,21 @@
 
 package com.floragunn.searchguard.test.helper.rest;
 
-import com.floragunn.searchguard.test.helper.certificate.TestCertificate;
-import nl.altindag.ssl.SSLFactory;
-import nl.altindag.ssl.util.PemUtils;
-
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.X509ExtendedKeyManager;
 import javax.net.ssl.X509ExtendedTrustManager;
-import java.util.Optional;
+
+import com.floragunn.searchguard.test.helper.certificate.TestCertificate;
+
+import nl.altindag.ssl.SSLFactory;
+import nl.altindag.ssl.util.PemUtils;
 
 public class TestCertificateBasedSSLContextProvider implements SSLContextProvider {
 
     private final TestCertificate caCertificate;
     private final TestCertificate certificate;
 
-    public TestCertificateBasedSSLContextProvider(TestCertificate caCertificate,
-                                                  TestCertificate certificate) {
+    public TestCertificateBasedSSLContextProvider(TestCertificate caCertificate, TestCertificate certificate) {
         this.caCertificate = caCertificate;
         this.certificate = certificate;
     }
@@ -41,18 +40,18 @@ public class TestCertificateBasedSSLContextProvider implements SSLContextProvide
     public SSLContext getSslContext(boolean clientAuthentication) {
         X509ExtendedTrustManager trustManager = PemUtils.loadTrustMaterial(caCertificate.getCertificateFile().toPath());
 
-        SSLFactory.Builder builder = SSLFactory.builder()
-                .withTrustMaterial(trustManager);
+        SSLFactory.Builder builder = SSLFactory.builder().withTrustMaterial(trustManager);
 
         if (clientAuthentication) {
-            X509ExtendedKeyManager keyManager = Optional.ofNullable(certificate.getPrivateKeyPassword())
-                    .map(password -> PemUtils.loadIdentityMaterial(
-                            certificate.getCertificateFile().toPath(),
-                            certificate.getPrivateKeyFile().toPath(),
-                            password.toCharArray()))
-                    .orElse(PemUtils.loadIdentityMaterial(
-                            certificate.getCertificateFile().toPath(),
-                            certificate.getPrivateKeyFile().toPath()));
+            X509ExtendedKeyManager keyManager;
+
+            if (certificate.getPrivateKeyPassword() != null) {
+                keyManager = PemUtils.loadIdentityMaterial(certificate.getCertificateFile().toPath(), certificate.getPrivateKeyFile().toPath(),
+                        certificate.getPrivateKeyPassword().toCharArray());
+            } else {
+                keyManager = PemUtils.loadIdentityMaterial(certificate.getCertificateFile().toPath(), certificate.getPrivateKeyFile().toPath());
+            }
+
             builder.withIdentityMaterial(keyManager);
         }
 
