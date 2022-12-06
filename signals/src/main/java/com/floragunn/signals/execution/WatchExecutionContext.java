e853cae54b35a6bdfd05a28d6c72921290b4a731
Nils Bandener <9-nibix@users.noreply.git.floragunn.com>
2022-12-06 14:37:37 +0000

    Introduced ack page links as template vars
    Closes #122
    
    See merge request search-guard/search-guard-suite-enterprise!307

--- signals/src/main/java/com/floragunn/signals/execution/WatchExecutionContext.java
+++ signals/src/main/java/com/floragunn/signals/execution/WatchExecutionContext.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2019-2022 floragunn GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.floragunn.signals.execution;
 
 import java.util.HashMap;
@@ -9,6 +26,7 @@ import org.elasticsearch.script.ScriptService;
 
 import com.floragunn.signals.accounts.AccountRegistry;
 import com.floragunn.signals.watch.action.invokers.ActionInvocationType;
+import com.floragunn.signals.watch.action.invokers.ActionInvoker;
 import com.floragunn.signals.watch.common.HttpEndpointWhitelist;
 import com.floragunn.signals.watch.common.HttpProxyConfig;
 
@@ -26,25 +44,22 @@ public class WatchExecutionContext {
     private final SimulationMode simulationMode;
     private final HttpEndpointWhitelist httpEndpointWhitelist;
     private final HttpProxyConfig httpProxyConfig;
-
+    private final String frontendBaseUrl;
+    private final ActionInvoker actionInvoker;
+    
     public Map<String, Object> getMetadata() {
         return metadata;
     }
 
     public WatchExecutionContext(Client client, ScriptService scriptService, NamedXContentRegistry xContentRegistry, AccountRegistry accountRegistry,
-            ExecutionEnvironment scheduled) {
-        this(client, scriptService, xContentRegistry, accountRegistry, scheduled, ActionInvocationType.ALERT, null);
-    }
-
-    public WatchExecutionContext(Client client, ScriptService scriptService, NamedXContentRegistry xContentRegistry, AccountRegistry accountRegistry,
             ExecutionEnvironment executionEnvironment, ActionInvocationType actionInvocationType, WatchExecutionContextData contextData) {
         this(client, scriptService, xContentRegistry, accountRegistry, executionEnvironment, actionInvocationType, contextData, null,
-                SimulationMode.FOR_REAL, null, null);
+                SimulationMode.FOR_REAL, null, null, null, null);
     }
 
     public WatchExecutionContext(Client client, ScriptService scriptService, NamedXContentRegistry xContentRegistry, AccountRegistry accountRegistry,
             ExecutionEnvironment executionEnvironment, ActionInvocationType actionInvocationType, WatchExecutionContextData contextData,
-            WatchExecutionContextData resolvedContextData, SimulationMode simulationMode, HttpEndpointWhitelist httpEndpointWhitelist, HttpProxyConfig httpProxyConfig) {
+            WatchExecutionContextData resolvedContextData, SimulationMode simulationMode, HttpEndpointWhitelist httpEndpointWhitelist, HttpProxyConfig httpProxyConfig, String frontendBaseUrl, ActionInvoker actionInvoker) {
         this.client = client;
         this.scriptService = scriptService;
         this.xContentRegistry = xContentRegistry;
@@ -57,6 +72,8 @@ public class WatchExecutionContext {
         this.simulationMode = simulationMode;
         this.httpEndpointWhitelist = httpEndpointWhitelist;
         this.httpProxyConfig = httpProxyConfig;
+        this.frontendBaseUrl = frontendBaseUrl;
+        this.actionInvoker = actionInvoker;
     }
 
     public Client getClient() {
@@ -89,24 +106,40 @@ public class WatchExecutionContext {
         if (resolvedContextData != null) {
             result.put("resolved", resolvedContextData.getTemplateScriptParamsAsMap());
         }
+        
+        if (this.frontendBaseUrl != null && actionInvocationType == ActionInvocationType.ALERT && this.contextData.getWatch() != null) {
+            String tenant = this.contextData.getWatch().getTenant();
+            
+            if ("_main".equals(tenant)) {
+                tenant = "SGS_GLOBAL_TENANT";
+            }
+
+            result.put("ack_watch_link", this.frontendBaseUrl + "/app/searchguard-signals?sg_tenant=" + tenant + "#/watch/"
+                    + this.contextData.getWatch().getId() + "/ack/");
+
+            if (this.actionInvoker != null && this.actionInvoker.getName() != null) {
+                result.put("ack_action_link", this.frontendBaseUrl + "/app/searchguard-signals?sg_tenant=" + tenant + "#/watch/"
+                        + this.contextData.getWatch().getId() + "/ack/" + this.actionInvoker.getName() + "/");
+            }
+        }
 
         return result;
     }
 
-    public WatchExecutionContext with(WatchExecutionContextData contextData) {
+    public WatchExecutionContext with(WatchExecutionContextData contextData, ActionInvoker actionInvoker) {
         return new WatchExecutionContext(client, scriptService, xContentRegistry, accountRegistry, executionEnvironment, actionInvocationType,
-                contextData, resolvedContextData, simulationMode, httpEndpointWhitelist, httpProxyConfig);
+                contextData, resolvedContextData, simulationMode, httpEndpointWhitelist, httpProxyConfig, frontendBaseUrl, actionInvoker);
     }
 
     public WatchExecutionContext with(ActionInvocationType actionInvocationType) {
         return new WatchExecutionContext(client, scriptService, xContentRegistry, accountRegistry, executionEnvironment, actionInvocationType,
-                contextData, resolvedContextData, simulationMode, httpEndpointWhitelist, httpProxyConfig);
+                contextData, resolvedContextData, simulationMode, httpEndpointWhitelist, httpProxyConfig, frontendBaseUrl, actionInvoker);
     }
 
     public WatchExecutionContext clone() {
         return new WatchExecutionContext(client, scriptService, xContentRegistry, accountRegistry, executionEnvironment, actionInvocationType,
                 contextData != null ? contextData.clone() : null, resolvedContextData != null ? resolvedContextData.clone() : null, simulationMode,
-                httpEndpointWhitelist, httpProxyConfig);
+                httpEndpointWhitelist, httpProxyConfig, frontendBaseUrl, actionInvoker);
     }
 
     public WatchExecutionContextData getResolvedContextData() {
@@ -127,6 +160,10 @@ public class WatchExecutionContext {
 
     public HttpProxyConfig getHttpProxyConfig() {
         return httpProxyConfig;
+    }
+
+    public String getFrontendBaseUrl() {
+        return frontendBaseUrl;
     }
 
 }
