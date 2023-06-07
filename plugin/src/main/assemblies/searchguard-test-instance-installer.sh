10078cd5df60e0e311361662fc31580597d0ee7d
Nils Bandener <git@bndnr.de>
2023-06-07 11:10:45 +0200

    Bumped tlstool to 1.9.1

--- plugin/src/main/assemblies/searchguard-test-instance-installer.sh
+++ plugin/src/main/assemblies/searchguard-test-instance-installer.sh
@@ -25,7 +25,7 @@ SGSF_VERSION="${2:-$SG_VERSION_PRE}"
 SG_REPOSITORY="${3:-search-guard-flx-release}"
 SG_PLUGIN_NAME="search-guard-flx"
 SGCTL_VERSION="1.2.0"
-TLS_TOOL_VERSION="1.9.0"
+TLS_TOOL_VERSION="1.9.1"
 
 ARCH_DETECTED="$(arch)"
 ARCH="x86_64"
