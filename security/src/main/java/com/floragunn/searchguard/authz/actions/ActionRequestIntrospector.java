c4fe62b74515d665f311fee81c101cd06fd9cfa7
Kacper Trochimiak <kacper.trochimiak@eliatra.com>
2023-06-19 12:20:38 +0200

    replace java.util.*; with single class imports

--- security/src/main/java/com/floragunn/searchguard/authz/actions/ActionRequestIntrospector.java
+++ security/src/main/java/com/floragunn/searchguard/authz/actions/ActionRequestIntrospector.java
@@ -17,12 +17,19 @@
 
 package com.floragunn.searchguard.authz.actions;
 
-import java.rmi.Remote;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.regex.PatternSyntaxException;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
