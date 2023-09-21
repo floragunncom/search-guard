01f3a50858e726a66517499cce106a97ddab0559
Kacper Trochimiak <kacper.trochimiak@eliatra.com>
2023-09-21 11:56:24 +0200

    fix KibanaActionsProvider, add new test to FeMultiTenancyTenantAccessMapperTest

--- dlic-fe-multi-tenancy/src/main/java/com/floragunn/searchguard/enterprise/femt/KibanaActionsProvider.java
+++ dlic-fe-multi-tenancy/src/main/java/com/floragunn/searchguard/enterprise/femt/KibanaActionsProvider.java
@@ -6,7 +6,7 @@ import com.floragunn.searchguard.authz.actions.Actions;
 public class KibanaActionsProvider {
 
     private static final String KIBANA_WRITE_ACTION_NAME = "kibana:saved_objects/_/write";
-    private static final String KIBANA_READ_ACTION_NAME = "kibana:saved_objects/_/write";
+    private static final String KIBANA_READ_ACTION_NAME = "kibana:saved_objects/_/read";
 
     private KibanaActionsProvider() {
 
--- dlic-fe-multi-tenancy/src/test/java/com/floragunn/searchguard/enterprise/femt/FeMultiTenancyTenantAccessMapperTest.java
+++ dlic-fe-multi-tenancy/src/test/java/com/floragunn/searchguard/enterprise/femt/FeMultiTenancyTenantAccessMapperTest.java
@@ -14,12 +14,12 @@
 
 package com.floragunn.searchguard.enterprise.femt;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import com.floragunn.searchguard.authz.TenantManager;
 import com.floragunn.searchsupport.cstate.metrics.MetricsLevel;
-import org.junit.Assert;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import com.floragunn.codova.documents.DocNode;
@@ -31,6 +31,8 @@ import com.floragunn.searchguard.authz.config.Role;
 import com.floragunn.searchguard.configuration.CType;
 import com.floragunn.searchguard.configuration.SgDynamicConfiguration;
 import com.floragunn.searchguard.user.User;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class FeMultiTenancyTenantAccessMapperTest {
 
@@ -45,7 +47,7 @@ public class FeMultiTenancyTenantAccessMapperTest {
                         DocNode.of("all_access",
                                 DocNode.of("tenant_permissions",
                                         List.of(
-                                                ImmutableMap.of("tenant_patterns", List.of("*"), "allowed_actions", Arrays.asList("*"))))),
+                                                ImmutableMap.of("tenant_patterns", List.of("*"), "allowed_actions", List.of("*"))))),
                         CType.ROLES, null)
                 .get();
 
@@ -55,9 +57,41 @@ public class FeMultiTenancyTenantAccessMapperTest {
         RoleBasedTenantAuthorization tenantAuthorization = new RoleBasedTenantAuthorization(roles, emptyActionGroups, actions, tenantManager, MetricsLevel.NONE);
         FeMultiTenancyTenantAccessMapper mapper = new FeMultiTenancyTenantAccessMapper(tenantManager, tenantAuthorization, actions);
 
-        User user = User.forUser("test").searchGuardRoles("all_access").build();
+        User user = User.forUser("user_name").searchGuardRoles("all_access").build();
 
-        Assert.assertEquals(ImmutableMap.of("test", true, "my_tenant", true), mapper.mapTenantsAccess(user, ImmutableSet.of("all_access")));
+        Map<String, Boolean> accessToTenants = mapper.mapTenantsAccess(user, ImmutableSet.of("all_access"));
+        assertThat(accessToTenants, Matchers.aMapWithSize(3));
+        assertThat(accessToTenants, Matchers.hasEntry("my_tenant", true));
+        assertThat(accessToTenants, Matchers.hasEntry("test", true));
+        assertThat(accessToTenants, Matchers.hasEntry(user.getName(), true));
+    }
+
+    @Test
+    public void tenantMappingByName() throws Exception {
+        SgDynamicConfiguration<Role> roles = SgDynamicConfiguration
+                .fromMap(
+                        DocNode.of("access_to_some_tenants",
+                                DocNode.of("tenant_permissions",
+                                        List.of(
+                                                ImmutableMap.of("tenant_patterns", List.of("write_tenant"), "allowed_actions", List.of(KibanaActionsProvider.getKibanaWriteAction(actions).name())),
+                                                ImmutableMap.of("tenant_patterns", List.of("read_tenant"), "allowed_actions", List.of(KibanaActionsProvider.getKibanaReadAction(actions).name()))
+                                        ))),
+                        CType.ROLES, null)
+                .get();
+
+        ImmutableSet<String> tenants = ImmutableSet.of("write_tenant", "read_tenant", "another_tenant");
+
+        TenantManager tenantManager = new TenantManager(tenants);
+        RoleBasedTenantAuthorization tenantAuthorization = new RoleBasedTenantAuthorization(roles, emptyActionGroups, actions, tenantManager, MetricsLevel.NONE);
+        FeMultiTenancyTenantAccessMapper mapper = new FeMultiTenancyTenantAccessMapper(tenantManager, tenantAuthorization, actions);
+
+        User user = User.forUser("user_name").searchGuardRoles("access_to_some_tenants").build();
+
+        Map<String, Boolean> accessToTenants = mapper.mapTenantsAccess(user, ImmutableSet.of("access_to_some_tenants"));
+        assertThat(accessToTenants, Matchers.aMapWithSize(3));
+        assertThat(accessToTenants, Matchers.hasEntry("write_tenant", true));
+        assertThat(accessToTenants, Matchers.hasEntry("read_tenant", false));
+        assertThat(accessToTenants, Matchers.hasEntry(user.getName(), true));
     }
 
 }
