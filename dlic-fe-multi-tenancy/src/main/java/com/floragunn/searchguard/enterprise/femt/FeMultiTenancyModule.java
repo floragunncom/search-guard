e61e87e20146fed201d2439925ed12080b8845d3
Lukasz Soszynski <lsoszyn@gmail.com>
2024-01-17 15:24:30 +0100

    MT is enabled by default.

--- dlic-fe-multi-tenancy/src/main/java/com/floragunn/searchguard/enterprise/femt/FeMultiTenancyModule.java
+++ dlic-fe-multi-tenancy/src/main/java/com/floragunn/searchguard/enterprise/femt/FeMultiTenancyModule.java
@@ -76,7 +76,7 @@ public class FeMultiTenancyModule implements SearchGuardModule, ComponentStatePr
      * It's undocumented in the user docs.
      */
     private static final StaticSettings.Attribute<Boolean> UNSUPPORTED_SINGLE_INDEX_MT_ENABLED = StaticSettings.Attribute
-            .define("searchguard.unsupported.single_index_mt_enabled").withDefault(false).asBoolean();
+            .define("searchguard.unsupported.single_index_mt_enabled").withDefault(true).asBoolean();
 
     private static final Logger log = LogManager.getLogger(FeMultiTenancyModule.class);
 
