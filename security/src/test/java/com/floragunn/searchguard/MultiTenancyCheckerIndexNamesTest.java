e35024c1e62f3fb5e67ef4412e34127764c8b906
Lukasz Soszynski <lsoszyn@gmail.com>
2024-01-17 15:24:30 +0100

    MT is enabled by default.

--- security/src/test/java/com/floragunn/searchguard/MultiTenancyCheckerIndexNamesTest.java
+++ security/src/test/java/com/floragunn/searchguard/MultiTenancyCheckerIndexNamesTest.java
@@ -109,7 +109,10 @@ public class MultiTenancyCheckerIndexNamesTest extends TestCase {
     @Test
     public void shouldDetermineIfIndexIsRelatedToMultiTenancy() {
         IndexRepository indexRepository = Mockito.mock(IndexRepository.class);
-        MultiTenancyChecker checker = new MultiTenancyChecker(Settings.builder().build(), indexRepository);
+        Settings settings = Settings.builder()
+            .put("searchguard.multi_tenancy_bootstrap_check_enabled", true) //
+            .build();
+        MultiTenancyChecker checker = new MultiTenancyChecker(settings, indexRepository);
         IndexMetadata mock = Mockito.mock(IndexMetadata.class);
         Mockito.when(mock.getCreationVersion()).thenReturn(Version.V_8_3_1);
         ImmutableMap<String, IndexMetadata> indices = ImmutableMap.of(indexName, mock);
