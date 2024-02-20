78a1350599023f02bd7e2ee587058a490eaaa803
Kacper Trochimiak <kacper.trochimiak@eliatra.com>
2024-02-20 12:30:57 +0000

    add username field to data returned from /current_user/tenants API
    See merge request search-guard/search-guard-suite-enterprise!708

--- dlic-fe-multi-tenancy/src/main/java/com/floragunn/searchguard/enterprise/femt/tenants/AvailableTenantData.java
+++ dlic-fe-multi-tenancy/src/main/java/com/floragunn/searchguard/enterprise/femt/tenants/AvailableTenantData.java
@@ -5,14 +5,15 @@ import com.floragunn.fluent.collections.OrderedImmutableMap;
 
 import java.util.Map;
 
-record AvailableTenantData(boolean multiTenancyEnabled,  Map<String, TenantAccessData> tenants)
+record AvailableTenantData(boolean multiTenancyEnabled,  Map<String, TenantAccessData> tenants, String username)
     implements Document<TenantAccessData> {
 
     private final static String FIELD_ENABLED = "multi_tenancy_enabled";
     private final static String FIELD_TENANTS = "tenants";
+    private final static String FIELD_USERNAME = "username";
 
     @Override
     public Object toBasicObject() {
-        return OrderedImmutableMap.of(FIELD_ENABLED, multiTenancyEnabled,  FIELD_TENANTS, tenants);
+        return OrderedImmutableMap.of(FIELD_ENABLED, multiTenancyEnabled,  FIELD_TENANTS, tenants, FIELD_USERNAME, username);
     }
 }
