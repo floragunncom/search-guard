995c4e95de95669a883182661d27c42d053b11f7
Lukasz Soszynski <lsoszyn@gmail.com>
2024-05-21 14:57:13 +0200

    Please revert me soon. Diagnostic context pernamently enabled.

--- support/src/main/java/com/floragunn/searchsupport/diag/DiagnosticContext.java
+++ support/src/main/java/com/floragunn/searchsupport/diag/DiagnosticContext.java
@@ -40,10 +40,10 @@ import org.elasticsearch.tasks.Task;
 public final class DiagnosticContext {
     private static final Logger log = LogManager.getLogger(DiagnosticContext.class);
 
-    public static final Setting<Boolean> ACTION_STACK_ENABLED = Setting.boolSetting("searchguard.diagnosis.action_stack.enabled", false,
+    public static final Setting<Boolean> ACTION_STACK_ENABLED = Setting.boolSetting("searchguard.diagnosis.action_stack.enabled", true,
             Property.NodeScope);
 
-    public static final Setting<Boolean> ADD_EXTENDED_HEADERS_TO_LOG_CONTEXT = Setting.boolSetting("searchguard.logging.context.extended", false,
+    public static final Setting<Boolean> ADD_EXTENDED_HEADERS_TO_LOG_CONTEXT = Setting.boolSetting("searchguard.logging.context.extended", true,
             Property.NodeScope);
 
     public static final List<Setting<?>> SETTINGS = Arrays.asList(ACTION_STACK_ENABLED, ADD_EXTENDED_HEADERS_TO_LOG_CONTEXT);
