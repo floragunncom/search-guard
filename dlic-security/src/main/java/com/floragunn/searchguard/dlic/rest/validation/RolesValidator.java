353c731a88f886cc3ad8daa62e533713d867c182
Łukasz Soszyński <lukasz.soszynski@eliatra.com>
2024-05-30 08:35:29 +0000

    Roles with alias_permissions and data_stream_permissions undergo the validation process correctly.
    See merge request search-guard/search-guard-suite-enterprise!852

--- dlic-security/src/main/java/com/floragunn/searchguard/dlic/rest/validation/RolesValidator.java
+++ dlic-security/src/main/java/com/floragunn/searchguard/dlic/rest/validation/RolesValidator.java
@@ -44,6 +44,8 @@ public class RolesValidator extends AbstractConfigurationValidator {
         allowedKeys.put("exclude_cluster_permissions", DataType.ARRAY);
         allowedKeys.put("exclude_index_permissions", DataType.ARRAY);
 		allowedKeys.put("description", DataType.STRING);
+        allowedKeys.put("alias_permissions", DataType.ARRAY);
+        allowedKeys.put("data_stream_permissions", DataType.ARRAY);
 	}
 
     @Override
