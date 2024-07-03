835b4cba600d4b5798544f1c8457f48f82b0593f
Kacper Trochimiak <kacper.trochimiak@eliatra.com>
2024-07-03 10:33:40 +0200

    run_tests.sh -Dsg.tests.es_download_cache.dir=$(pwd)

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -11,5 +11,5 @@ chown -R es_test .
 if [ "$TEST_CLUSTER_TYPE" == "full" ]; then
   su es_test -c "mvn $MAVEN_CLI_OPTS -pl $MODULE test -Dsg.tests.use_ep_cluster=true -Dsg.tests.sg_plugin.file=$(realpath ./plugin/target/releases/search-guard-flx-elasticsearch-plugin-*SNAPSHOT*.zip) -Dsg.tests.es_download_cache.dir=$(pwd)"
 else
-  su es_test -c "mvn $MAVEN_CLI_OPTS -pl $MODULE test"
+  su es_test -c "mvn $MAVEN_CLI_OPTS -pl $MODULE test -Dsg.tests.es_download_cache.dir=$(pwd)"
 fi
