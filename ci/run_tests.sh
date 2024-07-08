43a869086b618ff383b2e7d6955996c1aa9dd9cf
Lukasz Soszynski <lukasz.soszynski@eliatra.com>
2024-07-08 17:50:49 +0200

    Revert "Script ci/run_tests.sh from branch alias-privileges used."
    This reverts commit a79b0b60694846774968d437c6eeee144f285a58.

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -4,17 +4,11 @@ set -e
 
 MODULE=$1
 MAVEN_CLI_OPTS="--batch-mode -s settings.xml"
-RUN_TESTS_COMMAND="mvn $MAVEN_CLI_OPTS -pl $MODULE test -Dsg.tests.es_download_cache.dir=$(pwd) -Dsg.tests.sg_plugin.file=$(realpath ./plugin/target/releases/search-guard-flx-elasticsearch-plugin-*SNAPSHOT*.zip)"
-
-useradd -m es_test
-chown -R es_test .
 
 if [ "$TEST_CLUSTER_TYPE" == "full" ]; then
-  RUN_TESTS_COMMAND="$RUN_TESTS_COMMAND -Dsg.tests.use_ep_cluster=true"
+  useradd es_test
+  chown -R es_test .
+  su es_test -c "mvn $MAVEN_CLI_OPTS -pl $MODULE test -Dsg.tests.use_ep_cluster=true -Dsg.tests.sg_plugin.file=$(realpath ./plugin/target/releases/search-guard-flx-elasticsearch-plugin-*SNAPSHOT*.zip) -Dsg.tests.es_download_cache.dir=$(pwd)"
 else
-  RUN_TESTS_COMMAND="$RUN_TESTS_COMMAND -Dsg.tests.use_ep_cluster=false"
+  mvn $MAVEN_CLI_OPTS -Drevision=$SNAPSHOT_REVISION -Delasticsearch.version=$ES_VERSION -pl $MODULE test
 fi
-
-echo "RUN TEST COMMAND $RUN_TESTS_COMMAND"
-
-su es_test -c "$RUN_TESTS_COMMAND"
