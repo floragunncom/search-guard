2d5f800fe6f6021cecf0af48006ac7b7625aacf4
Lukasz Soszynski <lukasz.soszynski@eliatra.com>
2024-08-15 20:33:15 +0200

    Tests for Field Masking for aliases and data stream

--- security/src/test/java/com/floragunn/searchguard/test/TestAlias.java
+++ security/src/test/java/com/floragunn/searchguard/test/TestAlias.java
@@ -49,6 +49,11 @@ public class TestAlias implements TestIndexLike {
         return this;
     }
 
+    @Override
+    public String toString() {
+        return "Test alias name '" + name + "'";
+    }
+
     public void create(Client client) {
         client.admin().indices().aliases(new IndicesAliasesRequest().addAliasAction(AliasActions.add().indices(getIndexNamesAsArray()).alias(name)))
                 .actionGet();
