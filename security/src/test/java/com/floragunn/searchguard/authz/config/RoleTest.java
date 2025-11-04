b72bdcb7962441088277f9243fb4783abe7c9b49
Kacper Trochimiak <kacper.trochimiak@eliatra.com>
2025-11-04 12:14:20 +0100

    add tests

--- security/src/test/java/com/floragunn/searchguard/authz/config/RoleTest.java
+++ security/src/test/java/com/floragunn/searchguard/authz/config/RoleTest.java
@@ -18,6 +18,10 @@
 package com.floragunn.searchguard.authz.config;
 
 import com.floragunn.searchguard.authz.actions.Actions;
+import com.floragunn.searchguard.test.helper.log.LogsRule;
+import com.floragunn.searchsupport.util.EsLogging;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 
 import com.floragunn.codova.documents.DocNode;
@@ -28,6 +32,13 @@ import com.floragunn.searchguard.configuration.ConfigurationRepository;
 import com.floragunn.searchguard.configuration.SgDynamicConfiguration;
 
 public class RoleTest {
+
+    @ClassRule
+    public static EsLogging esLogging = new EsLogging();
+
+    @Rule
+    public LogsRule logsRule = new LogsRule("com.floragunn.searchguard.authz.config.Role");
+
     @Test(expected = ConfigValidationException.class)
     public void indexExclusion_notSupported() throws Exception {
         SgDynamicConfiguration.fromMap(DocNode.parse(Format.YAML).from(//
@@ -54,4 +65,73 @@ public class RoleTest {
         ), CType.ROLES, new ConfigurationRepository.Context(null, null, null, null, null, Actions.forTests())).get();
     }
 
+    @Test
+    public void shouldLogValidationWarnings_indexPrivsAssignedAsClusterPrivs() throws Exception {
+        SgDynamicConfiguration.fromMap(DocNode.parse(Format.YAML).from("""
+                test_role1:
+                    cluster_permissions:
+                        - "indices:admin/index_as_cluster_priv"
+                        - "cluster:monitor/main"
+                        - "indices:data/read/search/template" #this one is a cluster priv
+                        - "*"
+                        - "indices:data/*"
+                """
+        ), CType.ROLES, new ConfigurationRepository.Context(null, null, null, null, null, Actions.forTests())).get();
+
+        logsRule.assertThatContainExactly(String.format("Following index permissions are assigned as cluster permissions: [%s]", "indices:admin/index_as_cluster_priv, indices:data/*"));
+    }
+
+    @Test
+    public void shouldLogValidationWarnings_clusterPrivsAssignedAsIndexPrivs() throws Exception {
+        SgDynamicConfiguration.fromMap(DocNode.parse(Format.YAML).from("""
+                test_role1:
+                    index_permissions:
+                        - index_patterns: ["*"]
+                          allowed_actions:
+                            - "indices:data/read/search/template" #this one is a cluster priv
+                            - "cluster:admin/*"
+                            - "cluster:monitor/cluster_as_index_priv"
+                            - "indices:monitor/settings/get"
+                            - "*"
+                """
+        ), CType.ROLES, new ConfigurationRepository.Context(null, null, null, null, null, Actions.forTests())).get();
+
+        logsRule.assertThatContainExactly(String.format("Following cluster permissions are assigned as index permissions: [%s]", "indices:data/read/search/template, cluster:admin/*, cluster:monitor/cluster_as_index_priv"));
+    }
+
+    @Test
+    public void shouldLogValidationWarnings_clusterPrivsAssignedAsAliasPrivs() throws Exception {
+        SgDynamicConfiguration.fromMap(DocNode.parse(Format.YAML).from("""
+                test_role1:
+                    alias_permissions:
+                        - alias_patterns: ["*"]
+                          allowed_actions:
+                            - "indices:data/read/search/template" #this one is a cluster priv
+                            - "cluster:admin/*"
+                            - "indices:monitor/settings/get"
+                            - "cluster:monitor/cluster_as_index_priv"
+                            - "*"
+                """
+        ), CType.ROLES, new ConfigurationRepository.Context(null, null, null, null, null, Actions.forTests())).get();
+
+        logsRule.assertThatContainExactly(String.format("Following cluster permissions are assigned as alias permissions: [%s]", "indices:data/read/search/template, cluster:admin/*, cluster:monitor/cluster_as_index_priv"));
+    }
+
+    @Test
+    public void shouldLogValidationWarnings_clusterPrivsAssignedAsDataStreamPrivs() throws Exception {
+        SgDynamicConfiguration.fromMap(DocNode.parse(Format.YAML).from("""
+                test_role1:
+                    data_stream_permissions:
+                        - data_stream_patterns: ["*"]
+                          allowed_actions:
+                            - "indices:data/read/search/template" #this one is a cluster priv
+                            - "cluster:admin/*"
+                            - "indices:monitor/settings/get"
+                            - "cluster:monitor/cluster_as_index_priv"
+                            - "*"
+                """
+        ), CType.ROLES, new ConfigurationRepository.Context(null, null, null, null, null, Actions.forTests())).get();
+
+        logsRule.assertThatContainExactly(String.format("Following cluster permissions are assigned as data stream permissions: [%s]", "indices:data/read/search/template, cluster:admin/*, cluster:monitor/cluster_as_index_priv"));
+    }
 }
