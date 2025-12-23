73eb00e89d342f4d68bdcf651a5cc903ea159072
Lukasz Soszynski <lukasz.soszynski@eliatra.com>
2025-12-23 16:46:02 +0100

    Add support for write target for alias for data stream for failure store

--- support/src/main/java/com/floragunn/searchsupport/meta/Component.java
+++ support/src/main/java/com/floragunn/searchsupport/meta/Component.java
@@ -1,8 +1,36 @@
 package com.floragunn.searchsupport.meta;
 
+import com.floragunn.fluent.collections.ImmutableMap;
+import org.elasticsearch.cluster.metadata.DataStreamAlias;
+import org.elasticsearch.cluster.metadata.ProjectMetadata;
+import org.elasticsearch.index.Index;
+
+import java.util.Optional;
+
 public enum Component {
 
-    NONE(""), FAILURES("failures");
+    NONE("") {
+        @Override
+        public Optional<Meta.IndexLikeObject> extractWriteTargetForDataStreamAlias
+                (ProjectMetadata project,
+                DataStreamAlias dataStreamAlias,
+                ImmutableMap.Builder<String, Meta.IndexLikeObject> nameMap) {
+            return Optional.ofNullable(dataStreamAlias.getWriteDataStream())
+                    .map(this::indexLikeNameWithComponentSuffix)
+                    .map(nameMap::get);
+        }
+    }, FAILURES("failures") {
+        @Override
+        public Optional<Meta.IndexLikeObject> extractWriteTargetForDataStreamAlias(
+                ProjectMetadata project,
+                DataStreamAlias dataStreamAlias,
+                ImmutableMap.Builder<String, Meta.IndexLikeObject> nameMap) {
+            Index writeFailureIndex = project.getIndicesLookup().get(dataStreamAlias.getAlias()).getWriteFailureIndex(project);
+            return Optional.ofNullable(writeFailureIndex)
+                    .map(Index::getName)
+                    .map(nameMap::get);
+        }
+    };
 
     public static final String COMPONENT_SEPARATOR = "::";
 
@@ -22,4 +50,7 @@ public enum Component {
             return indexLikeName.concat(COMPONENT_SEPARATOR).concat(componentSuffix);
         }
     }
+
+    public abstract Optional<Meta.IndexLikeObject> extractWriteTargetForDataStreamAlias(ProjectMetadata project, DataStreamAlias dataStreamAlias,
+            ImmutableMap.Builder<String, Meta.IndexLikeObject> nameMap);
 }
