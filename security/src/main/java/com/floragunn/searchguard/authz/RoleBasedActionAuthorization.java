8daab30834095cc0efc45d3bcd101b795e605ab6
Lukasz Soszynski <lukasz.soszynski@eliatra.com>
2026-01-21 16:49:18 +0100

    Log message added to the method createCheckTable for debugging purposes.

--- security/src/main/java/com/floragunn/searchguard/authz/RoleBasedActionAuthorization.java
+++ security/src/main/java/com/floragunn/searchguard/authz/RoleBasedActionAuthorization.java
@@ -236,6 +236,10 @@ public class RoleBasedActionAuthorization implements ActionAuthorization, Compon
         // The specialFailureStoreAction privilege only applies to failure store components.
         // Pre-mark data component cells as checked so that only failure store components require this privilege.
         checkTable.checkIf(Meta.IndexLikeObject::isAssociatedWithDataComponent, specialFailureStoreAction);
+
+        // TODO CS: change to trace
+        log.error("Check table after extension with {} is {}", Actions.SPECIAL_FAILURE_STORE_NAME, checkTable);
+
         return checkTable;
     }
 
