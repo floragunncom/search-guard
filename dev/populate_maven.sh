ad9649d82a90c59af9996492979e403ed551265c
Kacper Trochimiak <kacper.trochimiak@eliatra.com>
2026-03-10 15:04:23 +0100

    remove transport-netty4 from populate_maven.sh

--- dev/populate_maven.sh
+++ dev/populate_maven.sh
@@ -58,34 +58,3 @@ EOT
       mvn -s "$SCRIPT_DIR/../settings.xml" org.apache.maven.plugins:maven-deploy-plugin:3.0.0-M2:deploy-file -Dfile="$PATH_PREFIX/$module/$module-$VERSION.jar" -DpomFile=".tmp.$module-$VERSION.pom" -DrepositoryId="third-party" -Durl="https://maven.search-guard.com:443/third-party"
   fi
 done
-
-cat <<EOT > ".tmp.netty-$VERSION.pom"
-<?xml version="1.0" encoding="UTF-8"?>
-<project xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd" xmlns="http://maven.apache.org/POM/4.0.0"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-  <modelVersion>4.0.0</modelVersion>
-  <groupId>org.elasticsearch.plugin</groupId>
-  <artifactId>transport-netty4</artifactId>
-  <version>$VERSION</version>
-  <description>POM was created from install:install-file</description>
-  <licenses>
-    <license>
-      <name>Elastic License 2.0 (ELv2)</name>
-      <url>https://raw.githubusercontent.com/elastic/elasticsearch/master/licenses/ELASTIC-LICENSE-2.0.txt</url>
-      <distribution>repo</distribution>
-    </license>
-    <license>
-        <name>Server Side Public License (SSPL) version 1</name>
-        <url>https://www.mongodb.com/licensing/server-side-public-license</url>
-        <distribution>repo</distribution>
-    </license>
-  </licenses>
-</project>
-EOT
-
-if [[ $TARGET != "remote" ]];then
-    mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile="$PATH_PREFIX/transport-netty4/transport-netty4-$VERSION.jar" -DpomFile=".tmp.netty-$VERSION.pom"
-else
-    mvn -s "$SCRIPT_DIR/../settings.xml" org.apache.maven.plugins:maven-deploy-plugin:3.0.0-M2:deploy-file -Dfile="$PATH_PREFIX/transport-netty4/transport-netty4-$VERSION.jar" -DpomFile=".tmp.netty-$VERSION.pom" -DrepositoryId="third-party" -Durl="https://maven.search-guard.com:443/third-party"
-fi
-
